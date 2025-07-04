06d970d622747f7f041530477baa02f1448b3cfc
alxpg <alxpega@gmail.com>
2025-07-04 00:50:14 -0600

    reparar el firebase

--- control-permisos-uti/src/lib/firebase.ts
+++ control-permisos-uti/src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAJDK9ImtmUmxWdx56RO_dfIoioq3pIN0Y",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
